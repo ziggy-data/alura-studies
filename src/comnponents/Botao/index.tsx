0c5aacbafed5ce2e0a72c407860c3e0c1fc3224a
King <reinaldo.ab00@gmail.com>
2022-03-25 23:09:19 -0300

    adicionando botao

---
+++ src/comnponents/Botao/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Botao extends React.Component{
+    render(){
+        return (
+                <button>
+                    Botão
+                </button>
+            )
+    }
+    
+}
+
+export default Botao;
